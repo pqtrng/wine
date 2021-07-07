d46ebbdb1c3f902e09c8279e834f5991581d3d50
Pham, Trung <trung@email.com>
2021-07-07 10:14:20 +0700

    Finish create model

---
+++ src/data.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def get_data(data_path, test_size, seed):
+
+    # Load in the data
+    df = pd.read_csv(data_path)
+
+    # Split into train and test sections
+    y = df.pop("quality")
+    X_train, X_test, y_train, y_test = train_test_split(
+        df, y, test_size=test_size, random_state=seed)
+    return X_train, y_train, X_test, y_test
+
+
+if __name__ == '__main__':
+    get_data()
